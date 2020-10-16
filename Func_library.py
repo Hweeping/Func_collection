603599176c9af0f3fc2eb02943a33acbb3175476
Hweeping <hweeping_dnt@hotmail.com>
2020-10-16 14:44:13 +0800

    Add first function

---
+++ Func_library.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 17:41:14 2020
+
+@author: nghp
+"""
+
+###################################################################
+#####                                                         #####
+#####   Name:   reverseNumber                                 #####
+#####   Input:  n = number to be reversed                     #####
+#####   Output: The reversed of the input number              #####
+#####                                                         #####
+###################################################################
+def reverseNumber(n):
+    reversed_number = int(str(n)[::-1])
+    return reversed_number
