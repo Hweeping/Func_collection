0165d6c206172fbcd4995f86c9c47d19676d42c8
Hweeping <hweeping_dnt@hotmail.com>
2022-02-23 13:52:56 +0800

    Update Func_library.py

--- Func_library.py
+++ Func_library.py
@@ -15,3 +15,12 @@ Created on Thu Oct 15 17:41:14 2020
 def reverseNumber(n):
     reversed_number = int(str(n)[::-1])
     return reversed_number
+
+###################################################################
+#####                                                         #####
+#####   Name:   New Function                                  #####
+#####   Input:  n = number to be reversed                     #####
+#####   Output: The reversed of the input number              #####
+#####                                                         #####
+###################################################################
+print("running script")
